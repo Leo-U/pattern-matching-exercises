c5d78d724c83ce8636ce1f6e04063f4e29ea3151
Joel Howard <joelhoward2@gmail.com>
2023-01-05 18:27:00 -0500

    complete find_employees and process_orders

--- patterns.rb
+++ patterns.rb
@@ -154,4 +154,213 @@ end
 # puts replace_non_whitespace("Hello\n123\nWorld!")
 # puts replace_non_whitespace("Hello 123\tWorld!")
 
+def classify_number(integer)
+  case integer.to_s
+  in /^[1-9]\d*$/
+    puts "#{integer} is a positive number."
+  in /^-\d+$/
+    puts "#{integer} is a negative number."
+  in '0'
+    puts "#{integer} is zero."
 
+  end
+end
+
+# classify_number(555)
+# classify_number(-952)
+# classify_number(0)
+
+def determine_case(string)
+  case string
+  in /^[A-Z]+$/ 
+    puts "The string is all uppercase."
+  in /^[a-z]+$/
+    puts "The string is all lowercase."
+  in /^[a-zA-Z]+$/
+    puts "The string is a mix of uppercase and lowercase characters."
+  end
+end
+
+# determine_case("ABC")
+# determine_case("abc")
+# determine_case("AbC")
+
+def determine_even_odd(integer)
+  case integer.to_s
+  in /^\d*[02468]$/
+    puts "#{integer} is even."
+  in /^\d*[13579]$/
+    puts "#{integer} is odd."
+  end
+end
+
+# determine_even_odd(3)
+# determine_even_odd(4)
+
+def check_palindrome(string)
+  unspaced = string.gsub(/\s+/, "")
+  reversed = unspaced.reverse
+
+  case unspaced
+  when reversed
+    puts "'#{string}' is a palindrome."
+  else
+    puts "'#{string}' is not a palindrome."
+  end
+end
+
+# check_palindrome("nurses run")
+# check_palindrome("dad")
+# check_palindrome("dogshit")
+
+data = [
+  {
+    name: "Alice",
+    age: 25,
+    address: {
+      street: "123 Main St",
+      city: "Chicago",
+      state: "IL"
+    }
+  },
+  {
+    name: "Bob",
+    age: 32,
+    address: {
+      street: "456 Market St",
+      city: "New York",
+      state: "NY"
+    }
+  }
+]
+
+
+def extract_info(data)
+  result = []
+  data.each do |element|
+    case element
+    in {name: name, age: age, address: {city: city}}
+      result << {name: name, age: age, city: city}
+    end
+  end
+  result
+end
+
+# puts extract_info(data)
+
+
+
+
+# Write a method extract_info that takes a nested data structure as an argument and extracts certain values from it.
+# The data structure will consist of nested arrays and hashes, and may have the following form:
+# Your method should return a new array of hashes, with only the name, age, and city values extracted from the original data structure.
+
+records = {
+  Alice: {
+    age: 25,
+    salary: 45000,
+    position: "developer"
+  },
+  Bob: {
+    age: 32,
+    salary: 50000,
+    position: "manager"
+  },
+  Charlie: {
+    age: 28,
+    salary: 60000,
+    position: "director"
+  }
+}
+
+def find_employee(records, name)
+  check_me = records[name]
+  name = name.to_sym
+  case name
+  in check_me
+    records[name]
+  end
+end
+
+# puts find_employee(records, "Bob")
+
+
+
+orders = [
+  {
+    customer: {
+      name: "Alice",
+      address: {
+        street: "123 Main St",
+        city: "Chicago",
+        state: "IL"
+      }
+    },
+    items: [
+      {
+        name: "Laptop",
+        price: 999.99,
+        quantity: 1
+      },
+      {
+        name: "Mouse",
+        price: 29.99,
+        quantity: 2
+      }
+    ]
+  },
+  {
+    customer: {
+      name: "Bob",
+      address: {
+        street: "456 Market St",
+        city: "New York",
+        state: "NY"
+      }
+    },
+    items: [
+      {
+        name: "Keyboard",
+        price: 59.99,
+        quantity: 1
+      },
+      {
+        name: "Monitor",
+        price: 199.99,
+        quantity: 1
+      }
+    ]
+  }
+]
+
+# def process_orders(orders)
+#   total_revenue = 0
+#   total_items_ordered = 0
+#   orders.each do |order|
+#     order[:items].each do |item|
+#       total_revenue += item[:price] * item[:quantity]
+#       total_items_ordered += item[:quantity]
+#     end
+#   end
+#   "Total revenue: #{total_revenue}. Total items ordered: #{total_items_ordered}"
+# end
+
+
+# the corrected solution:
+
+def process_orders(orders)
+  total_revenue = 0
+  total_items_ordered = 0
+  orders.each do |order|
+    case order
+    in {items: items}
+      items.each do |item|
+        total_revenue += item[:price] * item[:quantity]
+        total_items_ordered += item[:quantity]
+      end
+    end
+  end
+  { total_items: total_items_ordered, total_revenue: total_revenue }
+end
+
+# puts process_orders(orders)
